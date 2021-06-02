07a81431cdb4d31d0b0c71e9e963b5cec312b93f
george <35461002238898@mail.ru>
2021-06-03 01:34:49 +0300

    create new projec using npm parcel, scssm, ramda

---
+++ new-npm/script.js
@@ -0,0 +1,8 @@
+// import {sum} from 'ramda';
+const R = require('ramda');
+
+// const res = sum([2,4,6,8,100,1]);
+// console.log(res);
+const res = R.sum([2,4,6,8,100,1, 4244]);
+
+console.log(res);
