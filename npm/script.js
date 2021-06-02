c71d600597d23344e5bd3a58c4c8dda0b4a3e930
george <35461002238898@mail.ru>
2021-06-02 21:37:04 +0300

    refactor code: take function sum from global object R

--- npm/script.js
+++ npm/script.js
@@ -1,4 +1,8 @@
-import {sum} from 'ramda';
+// import {sum} from 'ramda';
+const R = require('ramda');
 
-const res = sum([2,4,6,8,100,1]);
+// const res = sum([2,4,6,8,100,1]);
+// console.log(res);
+const res = R.sum([2,4,6,8,100,1, 4244]);
+
 console.log(res);
